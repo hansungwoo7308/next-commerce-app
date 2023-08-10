3ccabcfb725128956e300725fb2025f2bdb589f0
hansungwoo7308 <hansungwoo7308@gmail.com>
2023-08-10 23:27:08 +0900

    update

--- lib/server/controllers/productControllers.ts
+++ lib/server/controllers/productControllers.ts
@@ -13,11 +13,13 @@ export const getProducts = async (req: any, res: any, next: any) => {
   const queryInstance = new APIfeatures(Product.find({}), req.query);
   // filter
   queryInstance.filter(); // 쿼리의 필터조건설정
-  const filteredProducts = await queryInstance.queryProducts; // 쿼리(조회)
+  const filteredProducts = await queryInstance.queryProducts; // 쿼리
   const filteredProductCount = filteredProducts.length; // 필터링한 총 제품의 수
   // paginate
   queryInstance.paginate(); // 쿼리의 페이지네이션설정
-  const paginatedProducts = await queryInstance.queryProducts.clone(); // 쿼리(조회) 복사본(중복조회회피)
+  // 완전히 새로운 작업을 할 때 clone method를 사용한다.
+  // 이 메서드는 쿼리객체(queryInstance.queryProducts===Product.find().filter())의 복사본을 생성한다.
+  const paginatedProducts = await queryInstance.queryProducts.clone(); // 쿼리 - 복사본(중복조회회피)
   // out
   res.status(200).json({ products: paginatedProducts, pages: Math.ceil(filteredProductCount / 3) });
 };
