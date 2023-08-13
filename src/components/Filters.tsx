cd1a4216c66d1af04cb5b0e074fd6d5c937d3291
hansungwoo7308 <hansungwoo7308@gmail.com>
2023-08-13 11:10:18 +0900

    update

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -16,24 +16,14 @@ export default function Filters() {
   const twoRef: any = useRef();
   const oneRef: any = useRef();
   const [ratingsElements, setRatingsElements]: any = useState([]);
-  const [ratingsValues, setRatingsValues]: any = useState([]);
-  const addRatingsValue = (value: any) => {
-    setRatingsValues([...ratingsValues, value]);
-  };
-  const removeRatingsValue = (value: any) => {
-    const updatedState = ratingsValues.filter((v: any) => v !== value);
-    setRatingsValues(updatedState);
-  };
-  const handleClick = (e: any) => {
-    // input tag의 key, value
+  const [ratings, setRatings]: any = useState([]);
+  // category handler
+  const handleClickCategory = (e: any) => {
     const { name, value } = e.target;
-    // console.log({ name, value });
-
-    if (e.target.checked) addRatingsValue(value);
-    else removeRatingsValue(value);
-
-    // ratingsValues 배열에 이미 존재하면, 배열에서 제거
-    // setRatingsValues((prevState: any) => {
+    router.query = { ...router.query, [name]: value };
+    router.push({ pathname: router.pathname, query: router.query });
+    // ratings 배열에 이미 존재하면, 배열에서 제거
+    // setRatings((prevState: any) => {
     //   const newState = prevState.filter((v: any) => {
     //     if (v === value) {
     //       console.log({ v, value });
@@ -43,33 +33,31 @@ export default function Filters() {
     //   });
     //   return [...prevState, newState];
     // });
-    // ratingsValues.map((v: any) => console.log(v));
-    // for (let value of ratingsValues) {
+    // ratings.map((v: any) => console.log(v));
+    // for (let value of ratings) {
     //   console.log({ value });
     // }
-    // console.log({ ratingsValues });
-    // const newState = [...ratingsValues, value];
-    // setRatingsValues((prevState: any) => {
+    // console.log({ ratings });
+    // const newState = [...ratings, value];
+    // setRatings((prevState: any) => {
     //   console.log({ prevState });
     //   if (prevState === value) return prevState;
     //   return newState;
     // });
     // console.log({ updatedState });
-    // setRatingsValues(updatedState);
-
+    // setRatings(updatedState);
     // rating input elements 중에서 체크된 상태에 따라서, 상태변경을 해준다.
-    // ratingsValues.map((ratingsValue: any) => {
+    // ratings.map((ratingsValue: any) => {
     //   // // 중복필드는 패스한다.
     //   // if(ratingsValue===name) return
     //   // // 새
-    //   // return [...ratingsValues,value]
+    //   // return [...ratings,value]
     // });
     // ratingsElements.map((element: any) => {
     //   if (element.checked) {
-    //     setRatingsValues((state: any) => [...state, element.value]);
+    //     setRatings((state: any) => [...state, element.value]);
     //   }
     // });
-
     // query key 에서 중복필드를 검사한다.
     // if (name === "ratings") {
     //   for (let key in router.query) {
@@ -81,21 +69,20 @@ export default function Filters() {
     //     }
     //   }
     // }
-
-    router.query = { ...router.query, [name]: value };
-    // router.query = { ...router.query, [name]: value };
-    // router.push({ pathname: router.pathname, query: router.query });
   };
-  useEffect(() => {
-    console.log({ ratingsValues });
-    const test = ratingsValues.reduce((a: any, v: any, i: any) => {
-      if (i === 0) return v;
-      return a + "+" + v;
-    }, "");
-    console.log({ test });
-    router.query = { ...router.query, ratings: test };
-    router.push({ pathname: router.pathname, query: router.query });
-  }, [ratingsValues]);
+  // ratings handler
+  const addRatingsValue = (value: any) => {
+    setRatings([...ratings, value]);
+  };
+  const removeRatingsValue = (value: any) => {
+    const updatedState = ratings.filter((v: any) => v !== value);
+    setRatings(updatedState);
+  };
+  const handleClickRatings = (e: any) => {
+    const { value } = e.target;
+    if (e.target.checked) addRatingsValue(value);
+    else removeRatingsValue(value);
+  };
   useEffect(() => {
     // 관리할 input elements를 셋팅한다.
     setCategoryElements([allRef.current, electronicsRef.current, foodRef.current]);
@@ -108,25 +95,61 @@ export default function Filters() {
     ]);
   }, []);
   useEffect(() => {
+    const localStorageRatings = localStorage.getItem("ratings");
+    if (!localStorageRatings) return;
+    if (!ratings.length) {
+      console.log({ localStorageRatings });
+      const splittedRatings = localStorageRatings.split("+");
+      console.log({ splittedRatings });
+      setRatings(splittedRatings);
+    }
+  }, []); // 캐싱데이터로 로컬스테이트를 저장한다.
+  useEffect(() => {
+    console.log({ ratings });
+    // ratings state 없으면 작업종료한다.
+    if (!ratings.length) {
+      localStorage.removeItem("ratings");
+      delete router.query.ratings;
+      router.push({ pathname: router.pathname, query: router.query });
+      return;
+    }
+    // string 으로 변환한다.
+    const stringfiedRatings = ratings.reduce((a: any, v: any, i: any) => {
+      if (i === 0) return v;
+      return a + "+" + v;
+    }, "");
+    // localStorage 에 캐싱한다.
+    localStorage.setItem("ratings", stringfiedRatings);
+    // set the query
+    router.query = { ...router.query, ratings: stringfiedRatings };
+    // out
+    router.push({ pathname: router.pathname, query: router.query });
+    ratingsElements.map((element: any) => {
+      console.log({ element });
+      ratings.map((value: any) => {
+        console.log({ value });
+        if (element.value === value) element.checked = true;
+      });
+    });
+  }, [ratings]); // ratings 로컬상태를 string 으로 변환하고 캐싱한다.
+
+  useEffect(() => {
     // router query 가 없으면, 초기화한다.
     if (!router.query.category) {
       allRef.current.checked = true;
     }
-    if (!router.query.ratings) {
-      ratingsElements.map((elements: any) => {
-        elements.checked = false;
-      });
-    }
+    // if (!router.query.ratings) {
+    //   ratingsElements.map((elements: any) => {
+    //     elements.checked = false;
+    //   });
+    // }
   }, [router.query.category, router.query.ratings]);
   useEffect(() => {
     // element values 중에서 router query parameter 와 일치하면. element 를 체크해준다.
     categoryElements.map((element: any) => {
       if (element.value === router.query.category) element.checked = true;
     });
-    ratingsElements.map((element: any) => {
-      if (element.value === router.query.ratings) element.checked = true;
-    });
-  }, [categoryElements, ratingsElements]);
+  }, [categoryElements]);
   return (
     <Box>
       <div className="category">
@@ -139,7 +162,7 @@ export default function Filters() {
                 type="radio"
                 name="category"
                 value="all"
-                onClick={handleClick}
+                onClick={handleClickCategory}
                 defaultChecked
               />
               <span>All</span>
@@ -152,7 +175,7 @@ export default function Filters() {
                 type="radio"
                 name="category"
                 value="electronics"
-                onClick={handleClick}
+                onClick={handleClickCategory}
               />
               <span>Electronics</span>
             </label>
@@ -164,7 +187,7 @@ export default function Filters() {
                 type="radio"
                 name="category"
                 value="food"
-                onClick={handleClick}
+                onClick={handleClickCategory}
               />
               <span>Food</span>
             </label>
@@ -176,13 +199,25 @@ export default function Filters() {
         <ul>
           <li>
             <label>
-              <input ref={fiveRef} type="checkbox" name="ratings" value="5" onClick={handleClick} />
+              <input
+                ref={fiveRef}
+                type="checkbox"
+                name="ratings"
+                value="5"
+                onClick={handleClickRatings}
+              />
               <span>*****</span>
             </label>
           </li>
           <li>
             <label>
-              <input ref={fourRef} type="checkbox" name="ratings" value="4" onClick={handleClick} />
+              <input
+                ref={fourRef}
+                type="checkbox"
+                name="ratings"
+                value="4"
+                onClick={handleClickRatings}
+              />
               <span>****</span>
             </label>
           </li>
@@ -193,97 +228,38 @@ export default function Filters() {
                 type="checkbox"
                 name="ratings"
                 value="3"
-                onClick={handleClick}
+                onClick={handleClickRatings}
               />
               <span>***</span>
             </label>
           </li>
           <li>
             <label>
-              <input ref={twoRef} type="checkbox" name="ratings" value="2" onClick={handleClick} />
+              <input
+                ref={twoRef}
+                type="checkbox"
+                name="ratings"
+                value="2"
+                onClick={handleClickRatings}
+              />
               <span>**</span>
             </label>
           </li>
           <li>
             <label>
-              <input ref={oneRef} type="checkbox" name="ratings" value="1" onClick={handleClick} />
+              <input
+                ref={oneRef}
+                type="checkbox"
+                name="ratings"
+                value="1"
+                onClick={handleClickRatings}
+              />
               <span>*</span>
             </label>
           </li>
         </ul>
       </div>
-      <h1>RaringsValues : {JSON.stringify(ratingsValues)}</h1>
-
-      {/* <div className="ratings">
-        <h3>Ratings</h3>
-        <ul>
-          <li>
-            <label>
-              <input ref={fiveRef} type="radio" name="ratings" value="5" onClick={handleClick} />
-              <span>*****</span>
-            </label>
-          </li>
-          <li>
-            <label>
-              <input ref={fourRef} type="radio" name="ratings" value="4" onClick={handleClick} />
-              <span>****</span>
-            </label>
-          </li>
-          <li>
-            <label>
-              <input ref={threeRef} type="radio" name="ratings" value="3" onClick={handleClick} />
-              <span>***</span>
-            </label>
-          </li>
-          <li>
-            <label>
-              <input ref={twoRef} type="radio" name="ratings" value="2" onClick={handleClick} />
-              <span>**</span>
-            </label>
-          </li>
-          <li>
-            <label>
-              <input ref={oneRef} type="radio" name="ratings" value="1" onClick={handleClick} />
-              <span>*</span>
-            </label>
-          </li>
-        </ul>
-      </div> */}
-      {/* <div className="test">
-        <h3>Test</h3>
-        <ul>
-          <li>
-            <label>
-              <input type="checkbox" name="test" value="a" onClick={handleClick} />
-              <span>a</span>
-            </label>
-          </li>
-          <li>
-            <label>
-              <input type="checkbox" name="test" value="b" onClick={handleClick} />
-              <span>b</span>
-            </label>
-          </li>
-          <li>
-            <label>
-              <input type="checkbox" name="test" value="c" onClick={handleClick} />
-              <span>c</span>
-            </label>
-          </li>
-          <li>
-            <label>
-              <input type="checkbox" name="test" value="d" onClick={handleClick} />
-              <span>d</span>
-            </label>
-          </li>
-          <li>
-            <label>
-              <input type="checkbox" name="test" value="e" onClick={handleClick} />
-              <span>e</span>
-            </label>
-          </li>
-        </ul>
-      </div> */}
+      <h3>RaringsValues : {JSON.stringify(ratings)}</h3>
     </Box>
   );
 }
