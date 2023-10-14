d6e7581c9a5ce6ea22c21489629751975e40cc89
hansungwoo7308 <hansungwoo7308@gmail.com>
2023-10-14 20:01:06 +0900

    update

--- src/components/product/Option.tsx
+++ src/components/product/Option.tsx
@@ -9,7 +9,6 @@ interface Props {
 
 export default function Option({ setSelectedItems, selectedItems }: Props) {
   const optionListRef: any = useRef(null);
-  // const [selectedItem, setSelectedItem]: any = useState(null);
   const [quantity, setQuantity]: any = useState(1);
   const {
     register,
@@ -24,14 +23,12 @@ export default function Option({ setSelectedItems, selectedItems }: Props) {
   const handleClickOptionItem = (item: string) => {
     optionListRef.current.classList.remove("option-list-open");
     const newItem = { item, quantity };
-    // console.log({ newItem });
     setSelectedItems((state: any) => {
       const isDuplicated = state.find((v: any) => v.item === item) ? true : false;
-      // console.log({ state, item, isDuplicated });
       if (isDuplicated) return state;
-      return [...state, newItem];
+      const newState = [...state, newItem];
+      return newState;
     });
-    // setSelectedItems((state: any) => [...state, newItem]);
   };
   useEffect(() => {
     const handleClick = () => optionListRef.current.classList.remove("option-list-open");
@@ -40,39 +37,39 @@ export default function Option({ setSelectedItems, selectedItems }: Props) {
   }, []);
 
   // option count button
-  const handleDecrease = (e: any) => {
-    const currentQuantity = Number(getValues("quantity"));
-    // if (currentQuantity === 1) return;
-    setValue("quantity", currentQuantity - 1);
-    // useForm getValues로는 react dom에서 값 변경에 따른 dom제어가 되지 않기 때문에,
-    // react useState를 사용한다.
-    // setQuantity(currentQuantity - 1);
-  };
-  const handleIncrease = (selectedItem: any) => {
-    console.log({ selectedItem });
-    // const currentQuantity = Number(getValues("quantity"));
+  // const handleDecrease = (e: any) => {
+  //   const currentQuantity = Number(getValues("quantity"));
+  //   // if (currentQuantity === 1) return;
+  //   setValue("quantity", currentQuantity - 1);
+  //   // useForm getValues로는 react dom에서 값 변경에 따른 dom제어가 되지 않기 때문에,
+  //   // react useState를 사용한다.
+  //   // setQuantity(currentQuantity - 1);
+  // };
+  // const handleIncrease = (selectedItem: any) => {
+  //   console.log({ selectedItem });
+  //   // const currentQuantity = Number(getValues("quantity"));
 
-    // selectedItem.quantity += 1;
-    console.log({ selectedItems });
-    setSelectedItems((state: any) => {
-      const foundItem = state.find((v: any) => v.item === selectedItem.item);
-      if (!foundItem) return;
-      foundItem.quantity += 1;
-      return state;
-    });
+  //   // selectedItem.quantity += 1;
+  //   console.log({ selectedItems });
+  //   setSelectedItems((state: any) => {
+  //     const foundItem = state.find((v: any) => v.item === selectedItem.item);
+  //     if (!foundItem) return;
+  //     foundItem.quantity += 1;
+  //     return state;
+  //   });
 
-    // selectedItems.map((state: any) => {
-    //   console.log({ state });
-    //   // const foundItem = state.find((v: any) => v.item === selectedItem);
-    //   // console.log({ foundItem });
-    // });
+  //   // selectedItems.map((state: any) => {
+  //   //   console.log({ state });
+  //   //   // const foundItem = state.find((v: any) => v.item === selectedItem);
+  //   //   // console.log({ foundItem });
+  //   // });
 
-    // setValue("quantity", currentQuantity + 1);
-    // setQuantity(currentQuantity + 1);
-    // setSelectedItems((state: any) => {
-    //   // console.log({ state });
-    // });
-  };
+  //   // setValue("quantity", currentQuantity + 1);
+  //   // setQuantity(currentQuantity + 1);
+  //   // setSelectedItems((state: any) => {
+  //   //   // console.log({ state });
+  //   // });
+  // };
 
   return (
     <Box>
@@ -129,38 +126,39 @@ export default function Option({ setSelectedItems, selectedItems }: Props) {
       {
         // selectedItems?.length &&
         selectedItems?.map((selectedItem: any, index: number) => {
-          console.log({ test: selectedItem });
           return (
             <div key={index} className="selected-item-outer">
               <div className="selected-item-option">
-                <button onClick={() => handleDecrease(selectedItem)} disabled={quantity === 1}>
-                  -
-                </button>
-                <input
-                  // {...register("quantity")}
-                  type="number"
-                  defaultValue={1}
-                  value={selectedItem.quantity}
-                  // readOnly
-                  // onChange={}
-                />
-                {/* <h1>{selectedItem?.quantity}</h1> */}
                 <button
                   onClick={() =>
                     setSelectedItems((state: any) => {
-                      const foundItem = state.find((v: any) => v.item === selectedItem.item);
-                      if (!foundItem) return;
-                      // foundItem.quantity += 1;
-                      // const newState = [...state].map((v: any) => {
-                      //   if (v.item === selectedItem.item) {
-                      //     v.quantity += 1;
-                      //   }
-                      // });
                       const newState = [...state];
-                      console.log({ newState });
+                      newState.find((v: any) => v.item === selectedItem.item).quantity -= 1;
+                      return newState;
+                    })
+                  }
+                  disabled={selectedItem.quantity === 1}
+                >
+                  -
+                </button>
+                <input
+                  type="number"
+                  defaultValue={1}
+                  value={selectedItem.quantity}
+                  onChange={(e) =>
+                    setSelectedItems((state: any) => {
+                      const newState = [...state];
+                      newState.find((v: any) => v.item === selectedItem.item).quantity =
+                        e.target.value;
+                      return newState;
+                    })
+                  }
+                />
+                <button
+                  onClick={() =>
+                    setSelectedItems((state: any) => {
+                      const newState = [...state];
                       newState.find((v: any) => v.item === selectedItem.item).quantity += 1;
-                      // console.log({ newState });
-                      // return [...state, foundItem];
                       return newState;
                     })
                   }
